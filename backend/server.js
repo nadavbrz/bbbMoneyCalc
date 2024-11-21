1c401a2a04e74ba07d4eec94f292bf355f2ece32
nadavbrz <nadavbrz@gmail.com>
2024-11-21 10:01:19 +0200

    try to fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'http://localhost:5174' })); 
+
 app.use(express.json());
 
 mongoConnect.connectToMongo();
